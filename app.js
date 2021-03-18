aee5e78c016ff493ddd7a587038fc5aa055a6c64
meghrathod <msr.megh@gmail.com>
2021-03-18 20:24:38 +0530

    formated code properly

--- app.js
+++ app.js
@@ -13,62 +13,63 @@ const homeStartingContent =
 const posts = [];
 
 app.get("/", function (req, res) {
-    res.render("home", {
-    });
+    res.render("home", {});
 });
-app.get('/about',function(req,res){
+app.get("/about", function (req, res) {
     res.render("about");
-  });
-  const contact=[{Name:'Lakshman Aakash',
-                EmailId:'la118@snu.edu.in',
-                PhoneNo:'6301368909',
-                photo:"images/imager.jpeg"
-               },
-               {Name:'Megh Rathod',
-                EmailId:'mr867@snu.edu.in',
-                PhoneNo:'9321582569',
-                photo:"images/imager.jpeg"
-               },
-               {Name:'Prem',
-                EmailId:'',
-                PhoneNo:'',
-                photo:"images/imager.jpeg"
-               },
-               {Name:'Raghav chaturvedi',
-                EmailId:'rc166@snu.edu.in',
-                PhoneNo:'9461494070',
-                photo: "images/imager.jpg"               
-
-               }
-            ];
-app.get("/contact",function(req,res){
-    res.render("contact",{l1:contact});
-  });
-  app.get('/blog',function(req,res){
-    res.render("blog",{l1:homeStartingContent,posts:posts});
 });
-  app.get('/compose',function(req,res){
+const contact = [
+    {
+        Name: "Lakshman Aakash",
+        EmailId: "la118@snu.edu.in",
+        PhoneNo: "6301368909",
+        photo: "images/imager.jpeg",
+    },
+    {
+        Name: "Megh Rathod",
+        EmailId: "mr867@snu.edu.in",
+        PhoneNo: "9321582569",
+        photo: "images/imager.jpeg",
+    },
+    { Name: "Prem", EmailId: "", PhoneNo: "", photo: "images/imager.jpeg" },
+    {
+        Name: "Raghav chaturvedi",
+        EmailId: "rc166@snu.edu.in",
+        PhoneNo: "9461494070",
+        photo: "images/imager.jpg",
+    },
+];
+app.get("/contact", function (req, res) {
+    res.render("contact", { l1: contact });
+});
+app.get("/blog", function (req, res) {
+    res.render("blog", { l1: homeStartingContent, posts: posts });
+});
+app.get("/compose", function (req, res) {
     res.render("compose");
-  })
-  app.post("/blog",function(req,res){
+});
+app.post("/blog", function (req, res) {
     res.redirect("/blog");
-  });
-    app.post('/compose',function(req,res){
-        const post={
-          title:req.body.composing,
-          content:req.body.postBody,
+});
+app.post("/compose", function (req, res) {
+    const post = {
+        title: req.body.composing,
+        content: req.body.postBody,
+    };
+    posts.push(post);
+    res.redirect("/");
+});
+app.get("/posts/:postName", function (req, res) {
+    const requestedTitle = _.lowerCase(req.params.postName);
+    for (let i = 0; i < posts.length; i++) {
+        if (_.lowerCase(posts[i].title) === requestedTitle) {
+            res.render("post", {
+                posting: posts[i].title,
+                compost: posts[i].content,
+            });
         }
-        posts.push(post);
-        res.redirect("/");
-      });
-  app.get("/posts/:postName",function(req,res){
-    const requestedTitle=_.lowerCase(req.params.postName);
-    for(let i=0;i<posts.length;i++){
-    if((_.lowerCase(posts[i].title))===requestedTitle){
-      res.render("post",{posting:posts[i].title,compost:posts[i].content});
     }
-  }
-  });
-app.listen(3000, function (req, res){
-    console.log("Listening on port 3000.")
-})
+});
+app.listen(3000, function (req, res) {
+    console.log("Listening on port 3000.");
+});
